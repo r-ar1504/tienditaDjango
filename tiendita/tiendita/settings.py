659d9a8c24f7f6517822ebd4b15e0e35e2f72ac0
rRios15041995 <rodolfo.r1504@gmail.com>
2017-05-11 18:34:50 -0500

    Changed settings

--- tiendita/tiendita/settings.py
+++ tiendita/tiendita/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
 	'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tiendita',
-        'USER': 'root',
-        'PASSWORD': 'djangodev',
+        'USER': 'testuser',
+        'PASSWORD': 'test123',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
