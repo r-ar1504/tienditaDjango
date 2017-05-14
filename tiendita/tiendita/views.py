6cad44590769740c0e8a4315a9c112635c782d46
rRios15041995 <rodolfo.r1504@gmail.com>
2017-05-14 15:13:49 -0500

    Added views

--- tiendita/tiendita/views.py
+++ tiendita/tiendita/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 def index_menu(request):
 	context = {}
 	return render(request, 'index.html', context)
+
+
+def login(request):
+	context = {}
+	return render(request, 'registration/login.html', context)
+
+def logout(request):
+	context = {}
+	return render(request, 'registration/logged_out.html', context)
